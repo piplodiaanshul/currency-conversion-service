a4e2fd7727b4ed95deba24c486d2dc47a209f627
piplodiaanshul <ansh.piplodia776@gmail.com>
2020-11-17 21:34:06 +0530

    added spring-cloud config-server client dependency

--- src/main/java/com/learner/microservices/currency_conversion/controllers/CurrencyConversionController.java
+++ src/main/java/com/learner/microservices/currency_conversion/controllers/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import com.learner.microservices.currency_conversion.feign_clients.ForexClient;
 import com.learner.microservices.currency_conversion.models.CurrencyConversionResponse;
 import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,14 @@ public class CurrencyConversionController {
 
   @Autowired
   private ForexClient forexClient;
+
+  @Value("${messages.welcome:Welcome}")
+  private String welcomeMessage;
+
+  @GetMapping("/welcome")
+  public String getWelcomeMessage() {
+    return welcomeMessage;
+  }
 
   @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionResponse convertCurrency(@PathVariable String from,
