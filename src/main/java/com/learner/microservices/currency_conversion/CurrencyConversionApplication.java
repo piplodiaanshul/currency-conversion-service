5db0bc6a8cdd0b84fa6989866d782c292ebd2a60
piplodiaanshul <ansh.piplodia776@gmail.com>
2020-11-18 20:09:05 +0530

    added swagger code for auto generated documentation

--- src/main/java/com/learner/microservices/currency_conversion/CurrencyConversionApplication.java
+++ src/main/java/com/learner/microservices/currency_conversion/CurrencyConversionApplication.java
@@ -1,16 +1,46 @@
 package com.learner.microservices.currency_conversion;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // to enable feign
 @SpringBootApplication
 @EnableFeignClients(basePackageClasses = {com.learner.microservices.currency_conversion.feign_clients.ForexClient.class})
+@EnableSwagger2
 public class CurrencyConversionApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CurrencyConversionApplication.class, args);
   }
 
+  @Bean  //configuring to show only selected apis in documentation
+  public Docket swaggerConfig() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors
+                  .basePackage("com.learner.microservices.currency_conversion.controllers"))
+        .build()
+        .apiInfo(getApiInfo());
+  }
+
+  private ApiInfo getApiInfo() {
+    Contact owner = new Contact("Anshul Piplodia", "", "ansh.piplodia776@gmail.com");
+    return new ApiInfo("Currency Conversion Apis",
+                       "All the apis related to currency conversion ",
+                       "1.0",
+                       "",
+                       owner,
+                       "",
+                       "",
+                       Collections.emptyList());
+  }
 }
--- src/main/java/com/learner/microservices/currency_conversion/controllers/CurrencyConversionController.java
+++ src/main/java/com/learner/microservices/currency_conversion/controllers/CurrencyConversionController.java
@@ -2,14 +2,17 @@ package com.learner.microservices.currency_conversion.controllers;
 
 import com.learner.microservices.currency_conversion.feign_clients.ForexClient;
 import com.learner.microservices.currency_conversion.models.CurrencyConversionResponse;
+import io.swagger.annotations.ApiOperation;
 import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class CurrencyConversionController {
 
   @Autowired
@@ -18,12 +21,19 @@ public class CurrencyConversionController {
   @Value("${messages.welcome:Welcome}")
   private String welcomeMessage;
 
-  @GetMapping("/welcome")
+  @ApiOperation(value = "Fetch welcome message",
+                notes = "Fetches a configurable welome message from config server",
+                response = String.class,
+                tags = {"configurable-greeting-message"})
+  @GetMapping("welcome")
   public String getWelcomeMessage() {
     return welcomeMessage;
   }
 
-  @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+  @ApiOperation(value = "Fetch converted amount",
+                notes = "Fetch the converted amount for the currency from Forex micro-service",
+                response = CurrencyConversionResponse.class)
+  @GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionResponse convertCurrency(@PathVariable String from,
                                                     @PathVariable String to,
                                                     @PathVariable BigDecimal quantity) {
