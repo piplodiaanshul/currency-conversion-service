0294189f3b2cb8f056a96be4883f28909ba034ba
piplodiaanshul <ansh.piplodia776@gmail.com>
2020-11-18 20:46:38 +0530

    added few more swagger notes

--- src/main/java/com/learner/microservices/currency_conversion/models/CurrencyConversionResponse.java
+++ src/main/java/com/learner/microservices/currency_conversion/models/CurrencyConversionResponse.java
@@ -1,17 +1,23 @@
 package com.learner.microservices.currency_conversion.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
+@ApiModel(value = "Currency conversion")
 public class CurrencyConversionResponse {
+  @ApiModelProperty(value = "Unique id")
   private Long id;
   private String from;
   private String to;
   private BigDecimal conversionMultiple;
   private BigDecimal quantity;
   private BigDecimal totalCalculatedAmount;
+  @ApiModelProperty(value = "Port number of Forex-service instance, so that we know the source od response")
   private int port;
+
   public CurrencyConversionResponse(Long id,
                                     String from,
                                     String to,
